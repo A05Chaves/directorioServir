2ac13a41a0cc1aff3811d291c2f75f0f15db021c
jachavesr <54504439+jachavesr@users.noreply.github.com>
2025-05-19 11:40:50 -0500

    se agrega campo apto a edicion y nuevo usuario 19 mayo

--- app_directorio/forms.py
+++ app_directorio/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from .models import Directorio
 
+
 class DirectorioForm(forms.ModelForm):
     class Meta:
         model = Directorio
-        fields = ['nombre_apellido', 'documento', 'parentesco', 'celular1', 'celular2', 'observacion']
+        fields = ['apto', 'nombre_apellido', 'documento', 'parentesco',
+                  'celular1', 'celular2', 'observacion']
         widgets = {
+            # Nuevo campo
+            'apto': forms.TextInput(attrs={'class': 'form-control'}),
             'nombre_apellido': forms.TextInput(attrs={'class': 'form-control'}),
             'documento': forms.TextInput(attrs={'class': 'form-control'}),
             'parentesco': forms.TextInput(attrs={'class': 'form-control'}),
@@ -14,11 +18,11 @@ class DirectorioForm(forms.ModelForm):
             'observacion': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
         }
 
-#CARGA EL ARCHIVO EN FORMATO EXCEL
+# CARGA EL ARCHIVO EN FORMATO EXCEL
+
 
 class CargaExcelForm(forms.Form):
     archivo_excel = forms.FileField(
         label='Subir archivo Excel',
         widget=forms.FileInput(attrs={'class': 'form-control'})
     )
-
