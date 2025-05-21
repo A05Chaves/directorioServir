5fa4e7d051f675c028da8f9f46620c215803db60
Alfredo <figo12747@gmail.com>
2025-05-20 21:17:19 -0500

    cambios por erroes en el codigo

--- app_directorio/forms.py
+++ app_directorio/forms.py
@@ -5,17 +5,11 @@ from .models import Directorio
 class DirectorioForm(forms.ModelForm):
     class Meta:
         model = Directorio
-<<<<<<< HEAD
-        fields = ['apto', 'nombre_apellido', 'documento', 'parentesco', 'celular1', 'celular2', 'observacion']
-        widgets = {
-            'apto':forms.TextInput( attrs={'class': 'form-control'}),
-=======
         fields = ['apto', 'nombre_apellido', 'documento', 'parentesco',
                   'celular1', 'celular2', 'observacion']
         widgets = {
             # Nuevo campo
             'apto': forms.TextInput(attrs={'class': 'form-control'}),
->>>>>>> 2ac13a41a0cc1aff3811d291c2f75f0f15db021c
             'nombre_apellido': forms.TextInput(attrs={'class': 'form-control'}),
             'documento': forms.TextInput(attrs={'class': 'form-control'}),
             'parentesco': forms.TextInput(attrs={'class': 'form-control'}),
